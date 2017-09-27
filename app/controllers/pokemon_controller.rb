1f6bc7906e95f840a47a4985798fb49c17137337
Marlon O'Neil <hello@marlononeil.com>
2017-09-27 18:10:38 -0400

    Completed task 'Adding the GIFs'

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,8 +1,8 @@
 class PokemonController < ApplicationController
 
   def show
-    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    body = JSON.parse(res.body)
+    pokemon_res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(pokemon_res.body)
     name  = body["name"]
     id  = body["id"]
     types = []
@@ -10,11 +10,18 @@ class PokemonController < ApplicationController
       types << type["type"]["name"]
     end
 
+    gilphy_res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=pikachu&rating=g")
+    body = JSON.parse(gilphy_res.body)
+    gif_url = body["data"][0]["url"]
+
     render json: {
       "id": id,
       "name": name,
-      "types": types
+      "types": types,
+      "gif": gif_url 
     }
+
+
 
   end
 
